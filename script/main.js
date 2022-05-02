07ebbe322e4d511d382216b67aea4cf0003644d6
Suuchann <suu_0219@naver.com>
2022-05-02 12:17:24 +0900

    popup carousel

--- script/main.js
+++ script/main.js
@@ -1,6 +1,14 @@
+let i = 1;
 $(function(){
+    popup();
     menu();
+    setInterval(carousel, 4000);
 })
+function popup(){
+    $("#popup button").click(function(){
+        $("#popup").hide()
+    });
+};
 function menu(){
     $("#gnb > li").mouseenter(function(){
         $(".gnb-bg").stop().animate({
@@ -14,4 +22,12 @@ function menu(){
         });
         $(".lnb").stop().slideUp();
     });
+};
+function carousel(){
+    if(i >= 3) i = 0;
+    let xPos = -1440 * i;
+    $(".img-wrap").animate({
+        marginLeft: xPos,
+    });
+    i++;
 };

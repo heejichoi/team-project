8b748483603ec3b5e3088bad9c3fd29ccb3c8c04
your-name-here <100328759+heejichoi@users.noreply.github.com>
2022-05-09 18:08:13 +0900

    리조트 마무리

--- public/script/main.js
+++ public/script/main.js
@@ -1,10 +1,42 @@
-let i = 0
+$(function(){
+    slide();
+    slide2();
 
-function carousel(){
-    if(i >= 3) i=0;
-    let yPos = -300* i;
-   $("#items").css({
-       marginTop : yPos
-   })
-   i++;
+})
+function slide(){
+    $(".right").on('click',function(){
+        let currItem = $("#items li.top")
+        let nextItem = currItem.next();
+        if(!nextItem.length) nextItem = $("#items li:first");
+        currItem.removeClass("top")
+        nextItem.addClass("top")
+
+    })
+    $(".left").on('click',function(){
+        let nowItem = $("#items li.top")
+        let prevItem = nowItem.prev();
+        if(!prevItem.length) prevItem = $("#items li:last");
+        nowItem.removeClass("top")
+        prevItem.addClass("top")
+
+    })
 }
+function slide2(){
+    $(".right2").on('click',function(){
+        let currItem = $("#items2 li.top")
+        let nextItem = currItem.next();
+        if(!nextItem.length) nextItem = $("#items2 li:first");
+        currItem.removeClass("top")
+        nextItem.addClass("top")
+
+    })
+    $(".left2").on('click',function(){
+        let nowItem = $("#items2 li.top")
+        let prevItem = nowItem.prev();
+        if(!prevItem.length) prevItem = $("#items2 li:last");
+        nowItem.removeClass("top")
+        prevItem.addClass("top")
+
+    })
+}
+
